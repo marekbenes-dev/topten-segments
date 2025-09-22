43c13461ad26f052e856054375439836561d15ee
marek.benes <benes.marek11@gmail.com>
2025-09-22 14:06:19 +0200

    unused code removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
-//import { useState } from "react";
 
 export default function Home() {
-  //const [authToken, setAuthToken] = useState<string | null>(null);
   const stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&%20response_type=force&scope=activity:read`;
 
   return (
