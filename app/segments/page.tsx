a795fb1a19bfef52bfd5632f2eae3ccc22f67681
marek.benes <benes.marek11@gmail.com>
2025-09-23 14:17:04 +0200

    body changed from JSON to params

--- app/segments/page.tsx
+++ app/segments/page.tsx
@@ -10,21 +10,31 @@ type StravaTokenResponse = {
 };
 
 async function exchangeCodeForToken(code: string) {
+  const { CLIENT_ID, CLIENT_SECRET } = process.env;
+
+  if (!CLIENT_ID || !CLIENT_SECRET) {
+    throw new Error("Missing CLIENT_ID or CLIENT_SECRET in Vercel environment properties.");
+  }
+
+  const params = new URLSearchParams({
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    code,
+    grant_type: "authorization_code",
+  });
+
   const res = await fetch("https://www.strava.com/api/v3/oauth/token", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: JSON.stringify({
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      code,
-      grant_type: "authorization_code",
-    }),
+    body: params,
     cache: "no-store",
   });
+
   if (!res.ok) {
     console.error("Strava token exchange failed", res);
-   throw new Error(`Token exchange failed, this is the response: ${JSON.stringify(res)}`); 
+    throw new Error(`Token exchange failed, this is the response: ${JSON.stringify(res)}`); 
   }
+  
   return (await res.json()) as StravaTokenResponse;
 }
 
